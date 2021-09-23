f3639ea1e9f7cae3829b411cfd46491556df9bc0
Hector Medina <hector.med27@gmail.com>
2021-09-22 22:28:00 -0700

    feat: introduce migration for image entity

---
+++ server/src/migrations/1632290555242-sample.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner, Table, TableColumn} from "typeorm";
+
+export class sample1632290555242 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn("cards", new TableColumn({
+            name: "description",
+            type: "varchar"
+        }))
+
+        await queryRunner.createTable(new Table({
+            name: "images",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true
+                },
+                {
+                    name: "url",
+                    type: "varchar"
+                },
+                {
+                    name: "upload_date",
+                    type: "timestamp",
+                    default: "now()"
+                },
+                {
+                    name: "card_id",
+                    type: "int",
+                    isNullable: false
+                }
+            ]
+        }), true);
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("cards", "description")
+
+        await queryRunner.dropTable("images", true)
+    }
+
+}
