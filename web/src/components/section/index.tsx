e466e43e90e31643b40062e4b6b2deca90b20a58
Kristea Gamel <krgamel@gmail.com>
2021-10-07 09:48:12 -0500

    feature: droppable
    adding droppable area to section

--- web/src/components/section/index.tsx
+++ web/src/components/section/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { Droppable, Draggable } from 'react-beautiful-dnd'
 import Card from '../card'
 import SectionI from '../../types/section'
 
@@ -32,50 +32,55 @@ const Section = ({
 
   return (
     <Wrapper>
-      <WrappedSection>
-        <SectionHeader>
-          <SectionTitle>{title}</SectionTitle>
-        </SectionHeader>
-        <CardsContainer>
-          {cards.length &&
-            cards.map((card: CardI) => {
-              return <Card key={card.id} card={card}></Card>
-            })}
-        </CardsContainer>
-        {isTempCardActive ? (
-          <CardComposerDiv>
-            <ListCardComponent>
-              <ListCardDetails>
-                <ListCardTextArea
-                  placeholder='Enter a title for the new card'
-                  onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
-                    setCardText(e.target.value)
-                  }
-                />
-              </ListCardDetails>
-            </ListCardComponent>
-            <SubmitCardButtonDiv>
-              <SubmitCardButton
-                type='button'
-                value='Add card'
-                onClick={(e: React.MouseEvent<HTMLElement>) => {
-                  e.preventDefault()
+      <Droppable droppableId={title}>
+        {(provided) => (
+          <WrappedSection>
+            <SectionHeader>
+              <SectionTitle>{title}</SectionTitle>
+            </SectionHeader>
+            <CardsContainer ref={provided.innerRef} {...provided.droppableProps}>
+              {cards.length &&
+                cards.map((card: CardI) => {
+                  return <Card key={card.id} card={card}></Card>
+                })}
+              {provided.placeholder}
+            </CardsContainer>
+            {isTempCardActive ? (
+              <CardComposerDiv>
+                <ListCardComponent>
+                  <ListCardDetails>
+                    <ListCardTextArea
+                      placeholder='Enter a title for the new card'
+                      onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+                        setCardText(e.target.value)
+                      }
+                    />
+                  </ListCardDetails>
+                </ListCardComponent>
+                <SubmitCardButtonDiv>
+                  <SubmitCardButton
+                    type='button'
+                    value='Add card'
+                    onClick={(e: React.MouseEvent<HTMLElement>) => {
+                      e.preventDefault()
 
-                  if (cardText) {
-                    onCardSubmit(id, cardText)
-                  }
+                      if (cardText) {
+                        onCardSubmit(id, cardText)
+                      }
 
-                  setIsTempCardActive(false)
-                }}
-              />
-            </SubmitCardButtonDiv>
-          </CardComposerDiv>
-        ) : (
-          <AddCardButtonDiv onClick={() => setIsTempCardActive(true)}>
-            <AddCardButtonSpan>Add another card</AddCardButtonSpan>
-          </AddCardButtonDiv>
+                      setIsTempCardActive(false)
+                    }}
+                  />
+                </SubmitCardButtonDiv>
+              </CardComposerDiv>
+            ) : (
+              <AddCardButtonDiv onClick={() => setIsTempCardActive(true)}>
+                <AddCardButtonSpan>Add another card</AddCardButtonSpan>
+              </AddCardButtonDiv>
+            )}
+          </WrappedSection>
         )}
-      </WrappedSection>
+      </Droppable>
     </Wrapper>
   )
 }
