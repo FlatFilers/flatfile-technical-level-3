ff4349c04c17cc2a7471d941e2c80fe8d734f258
Ivana Matijevic <ivmatij@gmail.com>
2021-12-12 16:26:42 -0500

    chore: disables cors to allow post requests

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+var allowlist = ['localhost']
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: true,
     cors: (req, callback) => {
+      console.log(req.headers.host)
       callback(null, {
-        origin: req.method !== 'POST',
+        origin: allowlist.indexOf(req.header('Origin')) !== -1 ? false : true,
         preflightContinue: false,
       })
     },
